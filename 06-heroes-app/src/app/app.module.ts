3a1edb830172d1e7768988534c5ddf72e2cfb973
Iván Machado <ivanmachado.dev@gmail.com>
2024-09-30 16:48:02 -0300

    Project HeroesApp started - Section 13 finished

---
+++ 06-heroes-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { SharedModule } from './../../../04-countries-app/src/app/shared/shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
