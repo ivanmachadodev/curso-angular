4e09d8b88d0a1c60b09d2ab8a34082286c475182
Iván Machado <ivanmachado.dev@gmail.com>
2024-10-31 14:49:49 -0300

    Se borra apiKey de Mapbox del modulo Maps y se hace refencia al environments.ts para obtener el dato

--- 10-maps-app/src/app/maps/maps.module.ts
+++ 10-maps-app/src/app/maps/maps.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaXZhbm1hY2hhZG9kZXYiLCJhIjoiY20yZjhneWRyMDZjcTJrbjF6cjZ3cnN5ZyJ9.Av5y8iClQSMRsI5DijRU-Q';
+mapboxgl.accessToken = environment.MAPBOX_KEY;
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
@@ -15,6 +14,7 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
 import { CounterAloneComponent } from "../alone/components/counter-alone/counter-alone.component";
+import { environment } from '../../environments/environments';
 
 
 @NgModule({
